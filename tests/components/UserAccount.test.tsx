6e81a42f1a049b023c24f7818a2bd47637e8eeeb
arswe <abdurrahmansoftw@gmail.com>
2024-02-28 12:43:42 +0600

    UserAccount not renders the edit button if  user is not admin

--- tests/components/UserAccount.test.tsx
+++ tests/components/UserAccount.test.tsx
@@ -6,7 +6,21 @@ describe('UserAccount', () => {
   it('renders the user profile name', () => {
     const user: User = { id: 1, name: 'arswe' }
     render(<UserAccount user={user} />)
-
     expect(screen.getByText(user.name)).toBeInTheDocument()
+  })
+
+  it('renders the edit button if is admin', () => {
+    const user: User = { id: 1, name: 'arswe', isAdmin: true }
+    render(<UserAccount user={user} />)
+    const button = screen.getByRole('button')
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent(/edit/i)
+  })
+
+  it('not renders the edit button if  user is not admin', () => {
+    const user: User = { id: 1, name: 'arswe' }
+    render(<UserAccount user={user} />)
+    const button = screen.queryByRole('button')
+    expect(button).not.toBeInTheDocument()
   })
 })
