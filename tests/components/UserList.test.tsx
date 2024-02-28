0afd9c872b41a641d8cdf2de8054962b3c928245
arswe <abdurrahmansoftw@gmail.com>
2024-02-28 12:55:00 +0600

    UserList

---
+++ tests/components/UserList.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import UserList from '../../src/components/accounts/UserList'
+import { User } from '../../src/entities/entities'
+
+describe('UserList', () => {
+  it('renders no users when the user array is empty', () => {
+    render(<UserList users={[]} />)
+    expect(screen.getByText(/no users available/i)).toBeInTheDocument()
+  })
+
+  it('renders a list of users', () => {
+    const users: User[] = [
+      { id: 1, name: 'arswe' },
+      { id: 2, name: 'john' },
+    ]
+    render(<UserList users={users} />)
+    users.forEach((user) => {
+      const link = screen.getByRole('link', { name: user.name })
+      expect(link).toBeInTheDocument()
+      expect(link).toHaveAttribute('href', `/users/${user.id}`)
+    })
+  })
+})
