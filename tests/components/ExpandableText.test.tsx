8439b9cd7d17f742542c26a5400f148d80a0a717
arswe <abdurrahmansoftw@gmail.com>
2024-02-29 10:53:40 +0600

    should render the full text if less then 255 charackters

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react'
-
+import { render } from '@testing-library/react'
+import ExpandableText from '../../src/components/ExpandableText'
 
 describe('ExpandableText', () => {
-  it('should render the text and a button', () => {
+  it('should render the full text if less then 255 charackters', () => {
     // Arrange
+    render(<ExpandableText text='short text' />)
     // Act
     // Assert
   })
