d4dbecdca74f102ad2622eba753685faa5c27172
arswe <abdurrahmansoftw@gmail.com>
2024-02-29 11:08:04 +0600

    should collapse text when show less button is clicked

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -22,11 +22,25 @@ describe('ExpandableText', () => {
 
   it('should expand text when show more button is clicked', async () => {
     render(<ExpandableText text={longText} />)
+
     const button = screen.getByRole('button')
     const user = userEvent.setup()
     await user.click(button)
 
     expect(screen.getByText(longText)).toBeInTheDocument()
     expect(button).toHaveTextContent(/less/i)
+  })
+
+  it('should collapse text when show less button is clicked', async () => {
+    render(<ExpandableText text={longText} />)
+    const showMoreButton = screen.getByRole('button', { name: /more/i })
+    const user = userEvent.setup()
+    await user.click(showMoreButton)
+
+    const showLessButton = screen.getByRole('button', { name: /less/i })
+    await user.click(showLessButton)
+
+    expect(screen.getByText(truncatedText)).toBeInTheDocument()
+    expect(showMoreButton).toHaveTextContent(/more/i)
   })
 })
