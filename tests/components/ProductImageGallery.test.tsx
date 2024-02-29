c97ea6df5e6cea5408c2a3b873acb74c9be6e04c
arswe <abdurrahmansoftw@gmail.com>
2024-02-29 10:30:51 +0600

    should render a list of image

--- tests/components/ProductImageGallery.test.tsx
+++ tests/components/ProductImageGallery.test.tsx
@@ -1,9 +1,20 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ProductImageGallery from '../../src/components/ProductImageGallery'
 
 describe('ProductImageGallery', () => {
   it('should render nothing if given an empty array', () => {
     const { container } = render(<ProductImageGallery imageUrls={[]} />)
     expect(container).toBeEmptyDOMElement()
+  })
+
+  it('should render a list of image', () => {
+    const imageUrls = ['ulr1', 'url2']
+    render(<ProductImageGallery imageUrls={imageUrls} />)
+    const images = screen.getAllByRole('img')
+    expect(images).toHaveLength(imageUrls.length)
+
+    imageUrls.forEach((url, index) => {
+      expect(images[index]).toHaveAttribute('src', url)
+    })
   })
 })
