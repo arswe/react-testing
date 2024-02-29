acb8ac03d8f6077b59354fa50e5a4f2fbbab9cdd
arswe <abdurrahmansoftw@gmail.com>
2024-02-29 10:59:33 +0600

    should truncate text if longer then 255 charackters

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -1,17 +1,19 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ExpandableText from '../../src/components/ExpandableText'
 
 describe('ExpandableText', () => {
   it('should render the full text if less then 255 charackters', () => {
-    // Arrange
-    render(<ExpandableText text='short text' />)
-    // Act
-    // Assert
+    const text = 'short text'
+    render(<ExpandableText text={text} />)
+    expect(screen.getByText(text)).toBeInTheDocument()
   })
 
-  it('should expand the text when the button is clicked', () => {
-    // Arrange
-    // Act
-    // Assert
+  it('should truncate text if longer then 255 charackters', () => {
+    const text = 'a'.repeat(256)
+    render(<ExpandableText text={text} />)
+    const truncatedText = `${text.slice(0, 255)}...`
+    expect(screen.getByText(truncatedText)).toBeInTheDocument()
+    const button = screen.getByRole('button')
+    expect(button).toHaveTextContent(/more/i)
   })
 })
