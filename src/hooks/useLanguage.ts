c04e2bfc73dc77b4fa6d23ca2b7d2b75eb21752c
arswe <abdurrahmansoftw@gmail.com>
2024-03-01 21:23:46 +0600

    useLanguage must be used within a LanguageProvider

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -3,6 +3,7 @@ import LanguageContext from '../providers/language/LanguageContext'
 
 const useLanguage = () => {
   const context = useContext(LanguageContext)
+  
   if (!context) throw new Error('useLanguage must be used within a LanguageProvider')
 
   return context
