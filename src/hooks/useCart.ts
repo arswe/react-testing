d9d8a1fd7e2c5f7acb0d1d7ee66a96e91ce94d75
arswe <abdurrahmansoftw@gmail.com>
2024-03-01 21:23:11 +0600

    useCart

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -3,6 +3,7 @@ import { CartContext } from '../providers/CartProvider'
 
 export const useCart = () => {
   const context = useContext(CartContext)
+  
   if (!context) throw new Error('useCart must be used within a CartProvider')
 
   return context
