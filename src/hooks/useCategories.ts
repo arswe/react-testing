dd5aa66f7a5020eb46cead0afafc32ef7542e506
arswe <abdurrahmansoftw@gmail.com>
2024-03-01 21:23:33 +0600

    useCategories

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -3,10 +3,12 @@ import { useQuery } from 'react-query'
 import { Category } from '../entities/entities'
 
 const useCategories = () => {
+
   return useQuery<Category[], Error>({
     queryKey: ['categories'],
     queryFn: () => axios.get('/categories').then((res) => res.data),
   })
+  
 }
 
 export default useCategories
