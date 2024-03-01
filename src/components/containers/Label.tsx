57ff4167d0874f30c2ef12edacebc53c2c53fedd
arswe <abdurrahmansoftw@gmail.com>
2024-03-01 21:21:57 +0600

    getLabel

--- src/components/containers/Label.tsx
+++ src/components/containers/Label.tsx
@@ -2,6 +2,7 @@ import { Text } from '@radix-ui/themes'
 import useLanguage from '../../hooks/useLanguage'
 
 const Label = ({ labelId }: { labelId: string }) => {
+  
   const { getLabel } = useLanguage()
 
   return <Text>{getLabel(labelId)}</Text>
