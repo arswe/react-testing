5a4779f8836cd2f3f160d4b84ef0196dd2905684
arswe <abdurrahmansoftw@gmail.com>
2024-03-01 21:18:24 +0600

    should render nothing if given an empty array

--- tests/components/ProductImageGallery.test.tsx
+++ tests/components/ProductImageGallery.test.tsx
@@ -3,14 +3,16 @@ import ProductImageGallery from '../../src/components/ProductImageGallery'
 
 describe('ProductImageGallery', () => {
   it('should render nothing if given an empty array', () => {
+    
     const { container } = render(<ProductImageGallery imageUrls={[]} />)
+
     expect(container).toBeEmptyDOMElement()
   })
 
   it('should render a list of image', () => {
     const imageUrls = ['ulr1', 'url2']
     render(<ProductImageGallery imageUrls={imageUrls} />)
-    
+
     const images = screen.getAllByRole('img')
     expect(images).toHaveLength(imageUrls.length)
 
